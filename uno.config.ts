54216c9558f4ddfe3131715abd12ead096a3bbbc
零 <tanchjim@aol.com>
2024-03-18 02:52:55 +0800

    add @apply dark-mode

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
 import presetMini from '@unocss/preset-mini'
-import type { DarkModeSelectors } from '@unocss/preset-mini'
 
 export default defineConfig({
     presets: [
